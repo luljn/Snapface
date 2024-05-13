3a4438ca04f5062977ac0a2d72a2b97f17d7d2fd
luljn <mbohlulajonathan4@gmail.com>
2024-05-13 20:36:26 +0200

    Add or remove a snap on a FaceSnap feature added

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -14,6 +14,8 @@ export class FaceSnapComponent implements OnInit {
   creationDate!: Date;
   snaps!: number;
   imageUrl!: string;
+  snapped!: boolean;
+  buttonText!: string;
 
   ngOnInit(): void {
       
@@ -22,5 +24,24 @@ export class FaceSnapComponent implements OnInit {
     this.creationDate = new Date();
     this.snaps = 8;
     this.imageUrl = 'https://picsum.photos/300/150?random=1';
+    this.snapped = false;
+    this.buttonText = 'Oh Snap !'
+  }
+
+  onAddOrRemoveSnap(){
+
+    if(this.snapped == true){
+
+      this.snaps--;
+      this.snapped = false;
+      this.buttonText = 'Oh Snap !';
+    }
+
+    else {
+
+      this.snaps++;
+      this.snapped = true;
+      this.buttonText = 'Oups UnSnap !'
+    }
   }
 }
