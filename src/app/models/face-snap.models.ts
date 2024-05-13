088c42f149fe210699580f3b2071a033b1e1d9d4
luljn <mbohlulajonathan4@gmail.com>
2024-05-13 20:51:45 +0200

    FaceSnap class added in models package

---
+++ src/app/models/face-snap.models.ts
@@ -0,0 +1,11 @@
+export class FaceSnap{
+
+    constructor(public title: string, 
+                public description: string, 
+                public creationDate: Date, 
+                public snaps: number, 
+                public imageUrl: string){
+
+        
+    }
+}
