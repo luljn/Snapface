58a68b3ffeebc0eab107451772b05befda4ec2ef
luljn <mbohlulajonathan4@gmail.com>
2024-05-16 22:07:43 +0200

    Application langage changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule, NgFor } from '@angular/common';
+import { Component, OnInit, LOCALE_ID } from '@angular/core';
+import { CommonModule, NgFor, registerLocaleData } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnap } from './models/face-snap.models';
+import * as fr from '@angular/common/locales/fr';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, FaceSnapComponent, CommonModule, NgFor],
+  providers : [
+    { provide: LOCALE_ID, useValue: 'fr-FR' }
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   
   facesnaps!: FaceSnap[];
+
+  constructor(){
+
+    registerLocaleData(fr.default);
+  }
 
   ngOnInit(): void {
       
