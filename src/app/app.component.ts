b9e9508cfc2702598eca8f920a6c0d4eedaee3fe
luljn <mbohlulajonathan4@gmail.com>
2024-05-18 22:38:45 +0200

    UI refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, LOCALE_ID } from '@angular/core';
 import { CommonModule, NgFor, registerLocaleData } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { HeaderComponent } from './header/header.component';
 import * as fr from '@angular/common/locales/fr';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FaceSnapComponent, FaceSnapListComponent,CommonModule, NgFor],
+  imports: [RouterOutlet, HeaderComponent ,FaceSnapListComponent, CommonModule, NgFor],
   providers : [
     { provide: LOCALE_ID, useValue: 'fr-FR' }
   ],
