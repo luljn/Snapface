82795e2a557b62fcaf1fdea8725a38f9637d636c
luljn <mbohlulajonathan4@gmail.com>
2024-05-24 21:23:48 +0200

    Memory leaks managed with high level Observables

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { FaceSnap } from '../models/face-snap.models';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
 import { FaceSnapsServices } from '../services/face-snaps.service';
+import { interval, Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -11,14 +12,26 @@ import { FaceSnapsServices } from '../services/face-snaps.service';
   templateUrl: './face-snap-list.component.html',
   styleUrl: './face-snap-list.component.scss'
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
 
   facesnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>
 
   constructor(private faceSnapsService: FaceSnapsServices) {}
 
   ngOnInit(): void {
-      
+    
+    this.destroy$ = new Subject<boolean>();
     this.facesnaps = this.faceSnapsService.getAllFaceSnaps();
+    
+    interval(1000).pipe(
+      takeUntil(this.destroy$),
+      tap(value => console.log(value))
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+      
+    this.destroy$.next(true);
   }
 }
