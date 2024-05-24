17d2c2142600a23df78681283cee742ddd3b6f68
luljn <mbohlulajonathan4@gmail.com>
2024-05-25 00:12:02 +0200

    Reactive Form upgraded, a preview of the new FaceSnap is added on the UI while the user is filling the form

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import { Observable, map } from 'rxjs';
+import { FaceSnap } from '../models/face-snap.models';
+import { NgIf, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-face-snap',
   standalone: true,
-  imports: [ ReactiveFormsModule ],
+  imports: [ ReactiveFormsModule, CommonModule ],
   templateUrl: './new-face-snap.component.html',
   styleUrl: './new-face-snap.component.scss'
 })
 export class NewFaceSnapComponent implements OnInit {
 
   snapForm!: FormGroup;
+  faceSnapPreview$!: Observable<FaceSnap>;
 
   constructor(private formBuilder: FormBuilder){}
 
@@ -22,6 +26,16 @@ export class NewFaceSnapComponent implements OnInit {
       imageUrl: [null],
       location: [null]
     });
+
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map(formValue => ({
+
+        ...formValue,
+        creationDate: new Date(),
+        id: 0,
+        snaps: 0,
+      }))
+    );
   }
 
   onSubmitForm(): void{
