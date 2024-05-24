50e27ee8c44267086ed291ef335278fe73601b48
luljn <mbohlulajonathan4@gmail.com>
2024-05-24 23:53:24 +0200

    Reactive Form added to create a new FaceSnap

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-new-face-snap',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './new-face-snap.component.html',
   styleUrl: './new-face-snap.component.scss'
 })
-export class NewFaceSnapComponent {
+export class NewFaceSnapComponent implements OnInit {
 
+  snapForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+      
+    this.snapForm = this.formBuilder.group({
+      title: [null],
+      description: [null],
+      imageUrl: [null],
+      location: [null]
+    });
+  }
+
+  onSubmitForm(): void{
+
+    console.log(this.snapForm.value);
+  }
 }
